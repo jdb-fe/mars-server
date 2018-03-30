07664c17a27667058a126e2d933ea4ab646b4a6f
bukas <bukas@bukas.local>
2018-03-30 17:03:35 +0800

    style: code style

--- src/post/post.entity.ts
+++ src/post/post.entity.ts
@@ -3,14 +3,23 @@ import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 @Entity()
 export class Post {
     @PrimaryGeneratedColumn() id: number;
+    // 标题
+    @Column({ length: 100 })
+    title: string;
+    // 描述
+    @Column()
+    description: string;
+    // 缩略图
+    @Column()
+    thumb: string;
+    // 内容
+    @Column('text')
+    content: string;
+    // 查看数量
+    @Column('int')
+    views: number;
+    // 状态
+    @Column()
+    status: boolean;
 
-    @Column({ length: 100 }) title: string;
-
-    @Column('text') description: string;
-
-    @Column() html: string;
-
-    @Column('int') views: number;
-
-    @Column() status: boolean;
 }
