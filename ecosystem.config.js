be436adc5a2392927b566a9a9cd2b6f3a9e53c33
bukas <bukas@bukas.local>
2018-04-24 16:53:19 +0800

    build: add deploy by pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    apps: [{
+        name: 'mars',
+        script: 'src/main.ts',
+        env: {
+            NODE_ENV: 'production',
+        },
+    }],
+    deploy: {
+        production: {
+            user: 'root',
+            host: 'ss.noonme.com',
+            ref: 'origin/master',
+            repo: 'git@github.com:jdb-fe/mars-server.git',
+            path: '/home/wwwroot/mars',
+            'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env production',
+        }
+    },
+};
