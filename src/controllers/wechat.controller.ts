6c0e116815269b1c91def17d0805b96f5489339a
huanz <260540233@qq.com>
2018-04-24 23:29:37 +0800

    fix: wechat mant times req

--- src/controllers/wechat.controller.ts
+++ src/controllers/wechat.controller.ts
@@ -7,6 +7,7 @@ import { PostService } from '../services/post.service';
 
 @Controller('wechat')
 export class WechatController {
+    busy = false
     constructor(
         private readonly parserService: ParserService,
         private readonly ruleService: RuleService,
@@ -18,10 +19,11 @@ export class WechatController {
         let msg:string|[any] = '直接发送文章链接即可推荐文章给我哦~~~';
         if (weixin.MsgType === 'text') {
             let input = (weixin.Content || '').trim();
-            if (validator.isURL(input)) {
+            if (validator.isURL(input) ) {
                 // 是否已经推荐了
                 let output:any = await this.postService.findByUrl(input);
-                if (!output) {
+                if (!output && !this.busy) {
+                    this.busy = true;
                     // 是否有规则
                     let parseRule = await this.ruleService.findByUrl(input);
                     try {
@@ -34,15 +36,18 @@ export class WechatController {
                         console.log(error);
                         output = await this.parserService.data(input);
                     }
+                    this.busy = false;
                     output.url = input;
                     await this.postService.insert(output);
                 }
-                msg = [{
-                    title: output.title,
-                    description: output.description,
-                    picurl: output.thumb,
-                    url: output.url
-                }];
+                if (!this.busy) {
+                    msg = [{
+                        title: output.title,
+                        description: output.description,
+                        picurl: output.thumb,
+                        url: output.url
+                    }];
+                }
             }
         }
         res.reply(msg);
